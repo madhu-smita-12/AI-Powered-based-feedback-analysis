408b3d30f670e6159724c5995562211fe6958cc9
madhu-smita-12 <madhusmitadey2007@gmail.com>
2026-07-25 10:30:38 +0530

    Added customer review dataset

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title("AI-Powered Aspect-Based Customer Feedback Analysis")
 
 st.write("Welcome to my AI Project!")
+# Read Dataset
+data = pd.read_csv("dataset/customer_reviews.csv")
+
+# Display Dataset
+st.subheader("Customer Reviews Dataset")
+st.dataframe(data)
